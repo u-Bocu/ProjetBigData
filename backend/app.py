d9946ffa7182c4972254a10a042cea14d8214a52
Mathieu Maisonnette <math.poke@gmail.com>
2023-03-19 18:59:19 +0100

    feat: Quiz api

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_cors import CORS
 
 from routes.themes import themes
+from routes.quiz import quiz
 
 
 def create_app():
@@ -10,6 +11,7 @@ def create_app():
 
     # Routing
     app.register_blueprint(themes, url_prefix='/api/themes')
+    app.register_blueprint(quiz, url_prefix='/api/quiz')
 
     if __name__ == "__main__":
         app.run(host="127.0.0.1", port=8080, debug=True)
