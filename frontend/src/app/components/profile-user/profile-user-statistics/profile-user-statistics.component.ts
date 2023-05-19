36613a9f12e24b3cbf8304ac0a7d7e1c8804d8fc
Julien MUGUET <julien-muguet@orange.fr>
2023-05-19 20:06:10 +0200

    [New] Page statistique non dynamique avec API

--- frontend/src/app/components/profile-user/profile-user-statistics/profile-user-statistics.component.ts
+++ frontend/src/app/components/profile-user/profile-user-statistics/profile-user-statistics.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { QuizService } from "../../../services/quiz.service";
+import { ThemeService } from "../../../services/theme.service";
+import { UserService } from '../../../services/user.service';
+import { LocalStorageService } from "../../../services/local-storage.service";
 
 @Component({
   selector: 'app-profile-user-statistics',
@@ -8,5 +12,57 @@ import { Component } from '@angular/core';
 export class ProfileUserStatisticsComponent {
   public loading: boolean = false;
 
-  constructor() {}
+  public kpi_best_quiz: Array<any> = [];
+  public kpi_worse_quiz: Array<any> = [];
+
+  public kpi_best_theme: Array<any> = [];
+  public kpi_worse_theme: Array<any> = [];
+
+  public kpi_avg_score: Array<any> = [];
+  public kpi_quiz_created: Array<any> = [];
+
+  constructor(
+    private quizService: QuizService,
+    private themeService: ThemeService,
+    private userService: UserService,
+    private localStorageService: LocalStorageService
+  ) { 
+    const idUser: number = parseInt(this.localStorageService.getData('user_id')) as unknown as number;
+    this.loading = true;
+    this.quizService.getBestQuiz(idUser).subscribe(response => {
+      this.kpi_best_quiz = response.data.rows;
+      this.loading = false;
+    });
+
+    this.loading = true;
+    this.quizService.getWorseQuiz(idUser).subscribe(response => {
+      this.kpi_worse_quiz = response.data.rows;
+      this.loading = false;
+    });
+
+    this.loading = true;
+    this.themeService.getBestTheme(idUser).subscribe(response => {
+      this.kpi_best_theme = response.data.rows;
+      this.loading = false;
+    });
+
+    this.loading = true;
+    this.themeService.getWorseTheme(idUser).subscribe(response => {
+      this.kpi_worse_theme = response.data.rows;
+      this.loading = false;
+    });
+
+    this.loading = true;
+    this.userService.getAvgScore(idUser).subscribe(response => {
+      this.kpi_avg_score = response.data.rows;
+      this.loading = false;
+    });
+
+    this.loading = true;
+    this.userService.getQuizCreated(idUser).subscribe(response => {
+      this.kpi_quiz_created = response.data.rows;
+      this.loading = false;
+    });
+  }
+
 }
--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -16,4 +16,20 @@ export class UserService {
       {headers: environment.HEADERS}
     );
   }
+
+  public getAvgScore(idUser?: number): Observable<any> {
+    const url = '/users/avg_score/' + idUser;
+    return  this.http.get(
+      environment.API_URL + url,
+      {headers: environment.HEADERS}
+    );
+  }
+
+  public getQuizCreated(idUser?: number): Observable<any> {
+    const url = '/users/quiz_created/' + idUser;
+    return  this.http.get(
+      environment.API_URL + url,
+      {headers: environment.HEADERS}
+    );
+  }
 }
